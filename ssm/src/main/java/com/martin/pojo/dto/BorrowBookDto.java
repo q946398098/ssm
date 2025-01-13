6619a0929dc472de22ff5ac3eecf8e6e7a3710de
Martin.Hu <Martin.Hu@ttigroup.com>
2025-01-13 17:35:31 +0800

    ---123add layui

--- ssm/src/main/java/com/martin/pojo/dto/BorrowBookDto.java
+++ ssm/src/main/java/com/martin/pojo/dto/BorrowBookDto.java
@@ -1,12 +1,8 @@
 package com.martin.pojo.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class BorrowBookDto {
     private int bookId;
     private String borrowerName;
