3c23f191d3e3e690e3718b0fad01be74bd63e8bb
Martin.Hu <Martin.Hu@ttigroup.com>
2025-01-17 17:38:53 +0800

    xixi

--- ssm/src/main/resources/sql/database.sql
+++ ssm/src/main/resources/sql/database.sql
@@ -12,3 +12,22 @@ CREATE TABLE `books` (
 INSERT INTO ssmbuild.books (bookId, bookName, bookCounts, detail, pictureUrl) VALUES(1, 'Mysql', 10, '这是课本1的详情', '/img/mysql.png');
 INSERT INTO ssmbuild.books (bookId, bookName, bookCounts, detail, pictureUrl) VALUES(2, 'Pgsql', 1, '这是课本1的详情', '/img/pgsql.png');
 INSERT INTO ssmbuild.books (bookId, bookName, bookCounts, detail, pictureUrl) VALUES(3, 'Oracle', 1, '这是课本1的详情', '/img/oracle.png');
+
+
+
+-- ssmbuild.`USER` definition
+
+CREATE TABLE `USER` (
+                        `cd_user` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+                        `ds_username` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+                        `ds_email` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                        `ds_sex` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                        `ds_city` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                        `ds_sign` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                        `nr_money` decimal(10,0) DEFAULT NULL,
+                        `ds_ip` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                        `nr_checkin` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+                        `dt_record` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                        `ds_password` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'MD5',
+                        UNIQUE KEY `cd_user` (`cd_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=148 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
